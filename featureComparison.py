815d0e2cb186ea915f644ddae2a8c08664450c00
Brandon Klein <33870950+Bklein19@users.noreply.github.com>
2019-02-06 13:54:25 -0500

    Create featureComparison.py
    Exists to compare two feature matrices of a phone. Returns only the features that the first element has that the second does not.

---
+++ featureComparison.py
@@ -0,0 +1,11 @@
+#compares two feature matrices and returns the notable feature differences the first has but the second doesn't.
+def featureComparison(matrix1, matrix2):
+    differentFeatures = []
+    for feature in matrix1:
+        containsfeature = False;
+        for otherFeature in matrix2:
+            if otherFeature == feature:
+                containsfeature = True;
+        if containsfeature == False:
+            differentFeatures.append(feature)
+    return differentFeatures
