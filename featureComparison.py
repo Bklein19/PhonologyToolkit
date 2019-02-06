acaa214176e1cae093cc89a63f8d7560c1e50c1e
Brandon Klein <33870950+Bklein19@users.noreply.github.com>
2019-02-06 15:51:27 -0500

    Update featureComparison.py
    added capability to deal with "n" matricies

--- featureComparison.py
+++ featureComparison.py
@@ -1,12 +1,17 @@
-#compares two feature matrices and returns the notable feature differences the first has but the second doesn't.
-def featureComparison(matrix1, matrix2):
+#compares n matrices and returns the notable feature differences that the first matrix has that 
+#are different from the others
+def featureComparison(matrices):
     differentFeatures = []
-    for feature in matrix1:
-        containsfeature = False;
-        for otherFeature in matrix2:
-            if otherFeature == feature:
-                containsfeature = True;
-                break
-        if containsfeature == False:
-            differentFeatures.append(feature)
+    matrixIndex = 0
+    for matrixIndex in range(len(matrices)):
+        for feature in matrices[0]:
+            containsfeature = False;
+            for otherFeature in matrices[matrixIndex]:
+                if otherFeature == feature:
+                    containsfeature = True;
+                    break
+            if containsfeature == False:
+                differentFeatures.append(feature)
+                differentFeatures.append(phoneFinder(matrices[matrixIndex]))
+        matrixIndex = matrixIndex + 1
     return differentFeatures
