1d3e643741c8fa73038fdc33fa756fcfd41a0702
Brandon Klein <33870950+Bklein19@users.noreply.github.com>
2019-02-22 21:00:11 -0500

    Update phoneFinder.py
    Added featureCreator functionality

--- phoneFinder.py
+++ phoneFinder.py
@@ -1,110 +1,7 @@
 #given a feature, returns all of the phones that have those features
 def phoneFinder(givenFeatures):
-    featureDict = {"p": ["+consonantal", "-sonorant", "-continuant", 
-                         "-delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "-spread glottis",
-                         "-constricted glottis", "+labial", "-round", "-labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"], 
-                   "b" : ["+consonantal", "-sonorant", "-continuant", 
-                         "-delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "+labial", "-round", "-labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   "m" : ["+consonantal", "+sonorant", "-continuant", 
-                         "0delayed release", "-approximant", "-tap", 
-                         "-trill", "+nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "+labial", "-round", "-labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   #labiodental
-                   "f" : ["+consonantal", "-sonorant", "+continuant", 
-                         "+delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "-spread glottis",
-                         "-constricted glottis", "+labial", "-round", "+labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   "v" : ["+consonantal", "-sonorant", "+continuant", 
-                         "+delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "+labial", "-round", "+labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   #alveolar
-                   "t" : ["+consonantal", "-sonorant", "-continuant", 
-                         "-delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "+coronal", "+anterior", "-distributed", "-strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   "d" : ["+consonantal", "-sonorant", "-continuant", 
-                         "-delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "+coronal", "+anterior", "-distributed", "-strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                  "s" : ["+consonantal", "-sonorant", "+continuant", 
-                         "+delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "+coronal", "+anterior", "-distributed", "+strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   #TO REPRESENT ESH SYMBOL IPA
-                   "esh" : ["+consonantal", "-sonorant", "+continuant", 
-                         "+delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "+coronal", "-anterior", "+distributed", "+strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                  "z" : ["+consonantal", "-sonorant", "+continuant", 
-                         "+delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "+coronal", "+anterior", "-distributed", "+strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                  "n" : ["+consonantal", "+sonorant", "-continuant", 
-                         "0delayed release", "-approximant", "-tap", 
-                         "-trill", "+nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "+coronal", "+anterior", "-distributed", "-strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                  "l" : ["+consonantal", "+sonorant", "+continuant", 
-                         "0delayed release", "+approximant", "-tap", 
-                         "-trill", "-nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "+labial", "-round", "-labiodental",
-                         "+coronal", "+anterior", "-distributed", "-strident", "+lateral",
-                         "-dorsal", "0ALLVOWEL"], 
-                   #velar
-                   "k" : ["+consonantal", "-sonorant", "-continuant", 
-                         "-delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "+dorsal", "+high", "-low", "0ALLVOWEL"],
-                   "g" : ["+consonantal", "-sonorant", "-continuant", 
-                         "-delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "+voice", "-spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "+dorsal", "+high", "-low", "0ALLVOWEL"],
-                   #glottal
-                   "h" : ["-consonantal", "-sonorant", "+continuant", 
-                         "+delayed release", "-approximant", "-tap", 
-                         "-trill", "-nasal", "-voice", "+spread glottis",
-                         "-constricted glottis", "-labial", "-round", "-labiodental",
-                         "-coronal", "0anterior", "0distributed", "0strident", "-lateral",
-                         "-dorsal", "0ALLVOWEL"],
-                   
-                   
-                  #vowels
-                  "a" : ["0ALLCONSONANT", "-high", "+low", "0tense", "-front", "-back", "-round"],
-                  "e" : ["0ALLCONSONANT", "-high", "-low", "+tense", "+front", "-back", "-round"],
-                  "i" : ["0ALLCONSONANT", "+high", "-low", "+tense", "+front", "-back", "-round"],
-                  "o" : ["0ALLCONSONANT", "-high", "-low", "+tense", "-front", "+back", "+round"],
-                  "u" : ["0ALLCONSONANT", "+high", "-low", "+tense", "-front", "+back", "+round"],}
     phonesWithTheFeature = []
-    for keyPhone, valueFeatures in featureDict.items():
+    for keyPhone, valueFeatures in featureCreator().items():
         goodPhoneValue = 0
         for f1 in valueFeatures:
             for f2 in givenFeatures:
@@ -113,3 +10,5 @@ def phoneFinder(givenFeatures):
         if goodPhoneValue == len(givenFeatures):
             phonesWithTheFeature.append(keyPhone)
     return phonesWithTheFeature
+        
+print(phoneFinder(["-sonorant", "+dorsal"]))
